90bcdacf4e78f1e5ecb7bb12a759341ef73499cc
MannanAnxari <mannananxari@gmail.com>
2023-01-26 22:49:44 +0500

    home slider change, services custom

--- src/Component/layout/Header.js
+++ src/Component/layout/Header.js
@@ -70,13 +70,17 @@ export const Header = () => {
                                     <div className='d-flex align-items-md-center gap-3 flex-md-row flex-column'>
                                         <div className="navbar-nav">
                                             <div className="nav-item dropdown">
-                                                <Link className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                                {/* <Link className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
                                                         <path fill="#212121" d="M11.5,8 C12.3284271,8 13,8.67157288 13,9.5 L13,10 C13,11.9714437 11.14049,14 8,14 C4.85950997,14 3,11.9714437 3,10 L3,9.5 C3,8.67157288 3.67157288,8 4.5,8 L11.5,8 Z M8,1.5 C9.51878306,1.5 10.75,2.73121694 10.75,4.25 C10.75,5.76878306 9.51878306,7 8,7 C6.48121694,7 5.25,5.76878306 5.25,4.25 C5.25,2.73121694 6.48121694,1.5 8,1.5 Z" />
                                                     </svg> {user ? user.data.name : "ClientArea"}
-                                                </Link>
+                                                </Link> */}
 
-                                                {user ?
+                                                <li class={`nav-item pl-4 pl-md-0 ml-0 ml-md-4`}>
+                                                    <Link class={`nav-link`} to={'//crm.mediachapter.us/login'}>ClientArea</Link>
+                                                </li>
+
+                                                {/* {user ?
 
                                                     <ul className="dropdown-menu">
 
@@ -100,8 +104,7 @@ export const Header = () => {
 
                                                                         <li><Link className="dropdown-item" to={'projects'}>Projects</Link></li>
                                                                         <li><Link className="dropdown-item" to={'invoices'}>Invoices</Link></li>
-                                                                        {/* <li><Link className="dropdown-item" to={'completed-projects'}>Completed Projects</Link></li> */}
-
+                                                                      
                                                                     </>
                                                                 }
 
@@ -119,7 +122,7 @@ export const Header = () => {
                                                         <li><Link className="dropdown-item" to={'//crm.mediachapter.us/login'}>Login</Link></li>
                                                         <li><Link className="dropdown-item" to={'//crm.mediachapter.us/signup'}>Signup</Link></li>
                                                     </ul>
-                                                }
+                                                } */}
                                             </div>
                                         </div>
                                         <Link to={'/contact'} className="btn btn-main text-uppercase">
